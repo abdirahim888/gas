916c25974856f133d23521a10a4f543532138cf8
andirahim <burcifiqi@gamil.com>
2025-03-05 10:33:12 +0300

    commit #2
    in the nv on line 9 Getter method to retrieve the user's name is added on line 11 Getter method to retrieve the user's age is added on line 14 Overriding the toString method to display user details is added

--- modular-java-app/user/src/user/user.java
+++
@@ -1,8 +0,0 @@
-package user;
-public class User {
-    private String name;
-    private int age;
-    public User(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
