aa6ce6de71e5f32c39bcf18c6f57b5b536c76a92
andirahim <burcifiqi@gamil.com>
2025-03-05 12:07:01 +0300

    commit #2
    in the nv on line 6 Method to add a user to the system is added on line  7 Creates and adds a new User object and on line 9  Method to retrieve all users

--- modular-java-app/user/src/user/UserService.java
+++ modular-java-app/user/src/user/UserService.java
@@ -3,3 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 public class UserService {
     private List<User> users = new ArrayList<>();
+    public void addUser(String name, int age) {
+        users.add(new User(name, age));
+    }
+    public List<User> getUsers() {
+        return users;
+    }
+}
