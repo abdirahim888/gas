d6b5ff68d3107f6db7ee77115370438607b14eac
andirahim <burcifiqi@gamil.com>
2025-03-05 13:25:06 +0300

    commit #2
    in the nv on line 6 Method to add an item to the system is added on line 7 Creates and adds a new Item object and on line 8 Method to retrieve all items is created

--- modular-java-app/Item/src/Item/ItemService.java
+++ modular-java-app/Item/src/Item/ItemService.java
@@ -3,3 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 public class ItemService {
     private List<Item> items = new ArrayList<>();
+    public void addItem(String name, double price) {
+        items.add(new Item(name, price));
+    }
+    public List<Item> getItems() {
+        return items;
+    }
+}
