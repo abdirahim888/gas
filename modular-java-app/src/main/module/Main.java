f74f8c9da4acf0a298d2f45fd872fd46d97cea2c
andirahim <burcifiqi@gamil.com>
2025-03-05 14:07:21 +0300

    commit #1
    in the nv on line 1 Defines the package for the Main class  , on line 2 Imports UserService to manage users is added, on line 3 Imports ItemService to manage items is added on line 4 Main class to run the application is created   on line 6&7 Creating instances of UserService and ItemService

---
+++ modular-java-app/src/main/module/Main.java
@@ -0,0 +1,8 @@
+package main.module;
+import user.UserService;
+import Item.ItemService;
+public class Main {
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        ItemService itemService = new ItemService();
+        
